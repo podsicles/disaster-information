83cd399db10abd02451fdcab345cd761ad7bd782
podsicles <jessicaflores.business@gmail.com>
2023-07-07 23:16:16 +0800

    add reference post and user

---
+++ app/models/post.rb
@@ -0,0 +1,14 @@
+class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :address, presence: true
+
+  has_many :comments
+  has_many :post_category_ships
+  has_many :categories, through: :post_category_ships
+  belongs_to :user
+
+  def destroy
+    update(deleted_at: Time.now)
+  end
+end
