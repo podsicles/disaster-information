e4684a8df4b597eaf16a553a87d7b8e72827e41c
podsicles <jessicaflores.business@gmail.com>
2023-07-07 10:29:22 +0800

    add flash messages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,8 +10,10 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(params[:post].permit(:title, :content, :address))
     if @post.save
+      flash[:notice] = 'Post created successfully'
       redirect_to posts_path
     else
+      flash.now[:alert] = 'Post create failed'
       render :new, status: :unprocessable_entity
     end
   end
