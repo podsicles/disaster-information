2d53148d372c619c88716f99dd5481c1900e3a78
podsicles <jessicaflores.business@gmail.com>
2023-07-07 10:24:29 +0800

    add methods new and create, and validate with error messages when failed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,17 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params[:post].permit(:title, :content, :address))
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
